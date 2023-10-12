31a48edc126170e50137d05c474ac3e2c63ac1e7
Michago6 <144434488+Michago6@users.noreply.github.com>
2023-10-12 16:30:33 +0200

    Update UppsalaDomkyrkan.py
    added stringToList function

--- UppsalaDomkyrkan.py
+++ UppsalaDomkyrkan.py
@@ -1,4 +1,23 @@
 print("Hello Uppsala")
 
-
-print("Yo")
+def stringToList(phrase):
+    #Funktionen tar ditt svar, separerar springen till en lista med alla ord i din mening
+    #listan är kronologisk, första ord till sista ord
+    #Gör alla orden till lower case.
+    #non-bokstäver karaktärer tas inte med! (#, !, .)
+    lst=[]
+    strng = []
+    forbiddenChars =["!",".",",","?"]
+    phrase = phrase + " "
+    for item in phrase:
+        if item == " ":
+            ord=""
+            for letter in strng:
+                ord=ord+letter.lower()
+            lst.append(ord)
+            strng.clear()
+        elif item in forbiddenChars:
+            pass
+        else:
+            strng.append(item)
+    return lst
